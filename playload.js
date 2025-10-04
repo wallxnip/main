2d139923f6b55df334542f649e8da06aa46e403a
Apps <wallxnip@gmail.com>
2025-10-04 03:06:45 -0300

    Atualizar o playload.js

--- playload.js
+++ playload.js
@@ -1,18 +1,16 @@
 const playlists = {
-onclick1: playlist1,
-
-drilland: playlist500,
-summer: playlist501,
-hiphop: playlist502,
-djen: playlist503,
-funk: playlist504,
-alone: playlist505,
-temporal: playlist506,
-
+  onclick1: playlist1,
+  drilland: playlist500,
+  summer: playlist501,
+  hiphop: playlist502,
+  djen: playlist503,
+  funk: playlist504,
+  alone: playlist505,
+  temporal: playlist506,
 };
-const playlistOrder = [
-'playlist504', // Funk (MIX)
-'playlist502', // Hip-Hop
-'playlist505', // Alone(EDM)
 
+const playlistOrder = [
+  'funk',   // Funk (MIX)
+  'hiphop', // Hip-Hop
+  'alone',  // Alone(EDM)
 ];
