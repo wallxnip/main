6e9e44586bc7dfd43d6bb5a7ac57909a32cdf811
Apps <wallxnip@gmail.com>
2025-10-04 06:25:57 -0300

    Atualizar o avatar.js

--- avatar.js
+++ avatar.js
@@ -1,3 +1,5 @@
-   
-// Exemplo de uso
-addAvatarWithFade('.avatar-container', 'https://i.postimg.cc/X7v3MS0g/1000385742.jpg', 'updateIt.html');
+addAvatarWithFade(
+'.avatar',
+'tymusic/ty.png',
+'updateIt.html'
+);
