8dd855002ed4c9b4df733c59b6f266b551cb4364
Apps <wallxnip@gmail.com>
2025-10-08 18:42:52 -0300

    Atualizar o intros.js

--- intros.js
+++ intros.js
@@ -57,28 +57,28 @@ createImageBlock('.playlist', [
 // -----------------------------------------
 createImageBlock('.player', [
   'app/splash/1.jpg',
-  'app/splash/2.jpg',
-  'app/splash/3.jpg',
-  'app/splash/4.jpg',
   'app/splash/5.jpg',
-  'app/splash/6.jpg',
-  'app/splash/7.jpg',
-  'app/splash/8.jpg',
   'app/splash/9.jpg',
+  'app/splash/4.jpg',
+  'app/splash/2.jpg',
+  'app/splash/6.jpg',
   'app/splash/10.jpg',
+  'app/splash/8.jpg',
+  'app/splash/3.jpg',
+  'app/splash/7.jpg',
 ], 16000);
 
 // -----------------------------------------
 // #Album
 // -----------------------------------------
 createImageBlock('.album', [
-  'app/splash/1.jpg',
-  'app/splash/2.jpg',
-  'app/splash/3.jpg',
-  'app/splash/4.jpg',
-  'app/splash/5.jpg',
-  'app/splash/6.jpg',
-  'app/splash/7.jpg',
+  'app/wallker/1.jpg',
+  'app/wallker/2.jpg',
+  'app/wallker/3.jpg',
+  'app/wallker/4.jpg',
+  'app/wallker/5.jpg',
+  'app/wallker/6.jpg',
+  'app/wallker/7.jpg',
   'app/splash/8.jpg',
   'app/splash/9.jpg',
   'app/splash/10.jpg',
