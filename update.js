5a83da40b0a1141378a0e2bf4521f17d26228f47
Apps <wallxnip@gmail.com>
2025-10-09 18:21:05 -0300

    Atualizar o update.js

--- update.js
+++ update.js
@@ -1,1 +1,12 @@
-div.innerHTML += '<a href=#" class="btn"> ATUALIZAR </a> ';
+    document.getElementById('updawn').addEventListener('click', () => {
+      // URL do arquivo no servidor
+      const url = 'https://seusite.com/update/app.apk';
+
+      // Cria um link temporário para forçar download
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'app.apk'; // nome do arquivo salvo
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    });
