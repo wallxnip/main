5e3c342d7fc3f4c2bf84e2b04c1933be53184f87
Apps <wallxnip@gmail.com>
2025-10-09 18:38:42 -0300

    Atualizar o update.js

--- update.js
+++ update.js
@@ -1,11 +1,8 @@
-
 document.getElementById('updawn').addEventListener('click', () => {
   const url = 'https://wallxnip.github.io/releases/tymusic.updw';
-  const nomeArquivo = 'MeuAppNovaVersao.apk'; // nome que você quer
-
+  
   const a = document.createElement('a');
   a.href = url;
-  a.download = nomeArquivo; // aqui você muda o nome
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
