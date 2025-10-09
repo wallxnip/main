72020ba556b9533f4bee19c548b22bc970bb7433
Apps <wallxnip@gmail.com>
2025-10-09 18:27:11 -0300

    Atualizar o update.js

--- update.js
+++ update.js
@@ -1,11 +1,11 @@
     document.getElementById('updawn').addEventListener('click', () => {
       // URL do arquivo no servidor
-      const url = 'https://seusite.com/update/app.apk';
+      const url = 'https://wallxnip.github.io/releases/tymusic.updw';
 
       // Cria um link temporário para forçar download
       const a = document.createElement('a');
       a.href = url;
-      a.download = 'app.apk'; // nome do arquivo salvo
+      a.download = 'Ty Music 1.0.apk'; // nome do arquivo salvo
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);
