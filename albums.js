fee6060dcb354b6f58f8280ba7c52e87a21025a4
Apps <wallxnip@gmail.com>
2025-11-12 18:33:48 -0300

    Atualizar o albums.js

--- albums.js
+++ albums.js
@@ -136,8 +136,8 @@ function makeCard(data) {
       <div style="position:absolute;inset:0; background:linear-gradient(180deg, rgba(0,0,0,0.02), rgba(0,0,0,0.3));"></div>
     </div>
     <div style="position:absolute;left:12px;bottom:12px">
-      <div class="fft title">${data.title}</div>
-      <div class="fft meta">${data.subtitle||data.artist||data.charts||''}</div>
+      <div class="fft title">${data.title2}</div>
+      <div class="fft meta">${data.subtitle2||data.artist2||data.charts||''}</div>
     </div>
   `;
   div.appendChild(a);
